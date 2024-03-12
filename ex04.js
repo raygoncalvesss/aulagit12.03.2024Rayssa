90a41bf0791f3d1b7467a7b38fb19870c10f1ce2
Rayssa Goncalves <rayssagoncalves.tds2.senai@gmail.com>
2024-03-12 16:37:57 -0300

    mudei o sinal de operação e as variaveis

--- ex04.js
+++ ex04.js
@@ -1,7 +1,7 @@
 //fazendo uma operação de soma
-let a = 25;
-let b = 55;
-let resultado = a + b;
+let a = 123;
+let b = 372;
+let resultado = a * b;
 let mensagem = "o resultado da operação é:";
 console.log(mensagem + resultado);
 //operação realizada na maquina por rayssa
